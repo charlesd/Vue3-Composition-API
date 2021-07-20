8cdc97e8827b675e67bb67c79b3311db5797bed3
charlesd <nicmiu@gmail.com>
2021-07-20 17:27:48 -0400

    Formatted

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
-  <HelloWorld url="https://restcountries.eu/rest/v2/regionalbloc/nafta" />
+	<HelloWorld url="https://restcountries.eu/rest/v2/regionalbloc/nafta" />
 </template>
 
 <script>
 import HelloWorldVue from "./components/HelloWorld.vue";
 export default {
-  name: "App",
-  components: {
-    HelloWorld: HelloWorldVue,
-  },
+	name: "App",
+	components: {
+		HelloWorld: HelloWorldVue,
+	},
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,73 +1,77 @@
 <template>
-  <div class="hello">
-	<table style="text-align: left">
-	  <thead>
-	    <tr>
-		  <th>Name</th>
-		  <th>Population</th>
-	    </tr>
-	  </thead>
-	  <tbody>
-	    <tr v-for="({name, population}, index) in countries" :key="index">
-		  <td>{{name}}</td>
-		  <td>{{population}}</td>
-	    </tr>
-	  </tbody>
-	</table>
-	<div style="text-align: left;">{{counter}} countries listed.</div>
-	<div style="text-align: left;">{{link}}</div>
-  </div>
+	<div class="hello">
+		<table style="text-align: left">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Population</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr
+					v-for="({ name, population }, index) in countries"
+					:key="index"
+				>
+					<td>{{ name }}</td>
+					<td>{{ population }}</td>
+				</tr>
+			</tbody>
+		</table>
+		<div style="text-align: left">{{ counter }} countries listed.</div>
+		<div style="text-align: left">{{ link }}</div>
+	</div>
 </template>
 
 <script>
-import { ref, watch, computed, onMounted, toRefs } from 'vue'
-import axios from 'axios'
+import { ref, watch, computed, onMounted, toRefs } from "vue";
+import axios from "axios";
 
 export default {
-  name: "HelloWorld",
-  props: {
-    url: {
-      type: String,
-      required: true
-    }
-  },
-  setup(props){
-	const { url } = toRefs(props)
-	const countries = ref([])
-	const counter = ref(0)
-	const link = computed(() => '*** Data source: '+url.value+' ***')
+	name: "HelloWorld",
+	props: {
+		url: {
+			type: String,
+			required: true,
+		},
+	},
+	setup(props) {
+		const { url } = toRefs(props);
+		const countries = ref([]);
+		const counter = ref(0);
+		const link = computed(() => "*** Data source: " + url.value + " ***");
 
-	watch(countries, (newValue, oldValue) => {
-	//	console.log('countries: ' + countries.value)
-		counter.value = newValue.length
-	})
-	onMounted(() => {
-		axios(url.value)
-			.then(response => countries.value = response.data)
-	})
-	return {
-		countries,
-		counter,
-		link
-	}
-  },
+		watch(countries, (newValue, oldValue) => {
+			//	console.log('countries: ' + countries.value)
+			counter.value = newValue.length;
+		});
+		onMounted(() => {
+			axios(url.value).then(
+				(response) => (countries.value = response.data)
+			);
+		});
+		return {
+			countries,
+			counter,
+			link,
+		};
+	},
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
-  margin: 40px 0 0;
+	margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+	list-style-type: none;
+	padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+	display: inline-block;
+	margin: 0 10px;
 }
 a {
-  color: #42b983;
+	color: #42b983;
 }
 </style>
